e269fe6bd22440d92516f569cbedc3fb32431eb0
Jami Couch <fbcouch@gmail.com>
2013-05-12 22:29:57 -0500

    Updated VERSION for nightly

--- src/com/ahsgaming/valleyofbones/VOBGame.java
+++ src/com/ahsgaming/valleyofbones/VOBGame.java
@@ -24,7 +24,7 @@ public class VOBGame extends Game {
     public static final boolean DEBUG_LOCK_SCREEN = true;
 	public static final String LOG = "VOBGame";
 
-    public static final int VERSION = 5;
+    public static final int VERSION = 6;
 
     FPSLogger fpsLogger = new FPSLogger();
 	
